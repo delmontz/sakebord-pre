631bcc00021a7b9754592f58b00b4efc16b90be8
delmontz <delmontz@dgl.tokyo>
2019-04-16 19:28:40 +0900

    add: init

---
+++ src/redux/modules/user.js
@@ -0,0 +1,9 @@
+import firebase from 'react-native-firebase';
+
+export default function getUser() {
+   firebase.auth().signInAnonymously().then((res) => {
+      console.log(res);
+   }).catch((error) => {
+      console.log(error);
+   });
+}
---
+++ src/screen/home/index.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import {
+   View,
+   Text,
+} from 'react-native';
+
+import getUser from '../../redux/modules/user'
+
+import styles from './styles';
+
+class HomeScreen extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         //
+      };
+   }
+
+   componentDidMount() {
+      getUser();
+   }
+
+   render() {
+      return (
+         <View style={styles.container}>
+         <Text>HOME SCREEN</Text>
+         <Text>Hello Hoge Hoge</Text>
+         </View>
+      );
+   }
+}
+
+export default HomeScreen;
